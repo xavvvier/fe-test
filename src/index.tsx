86a5c942c9d9f236fa1a25654d37a3874d9154f1
xavvvier <xavvvier@users.noreply.github.com>
2021-08-16 11:50:58 -0300

    set up webpack

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return <div>React is working!</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = function (_env, argv) {
+  const isDevelopment = argv.mode !== 'production';
+
+  return {
+    entry: './src/index.tsx',
+    devtool: isDevelopment && 'inline-source-map',
+    mode: isDevelopment && 'development',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /(node_modules)/,
+          loader: 'ts-loader',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.ts', '.tsx'],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'index.html',
+      }),
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, 'dist/'),
+      port: 3000,
+    },
+  };
+};
